4620c014c4d6666f7182b5cc57a20c7e12ed147d
razamughal333 <razahmedmughal@gmail.com>
2026-07-06 04:07:36 +0500

    updated Welcome.jpx file for my react app

---
+++ viteapp/my-react-app/src/Welcome.jsx
@@ -0,0 +1,10 @@
+function Welcome() {
+  return (
+    <div>
+      <h2 bg="light-green">Welcome to React!</h2>
+      <p bg="light-gray">This is my first reusable component.</p>
+    </div>
+  );
+}
+
+export default Welcome;
--- viteapp/my-react-app/src/main.jsx
+++ viteapp/my-react-app/src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import Counter from "./Counter.jsx";
+import Welcome from "./Welcome.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Welcome />
     <App />
     <Counter />
+    <Welcome />
   </StrictMode>,
 );
